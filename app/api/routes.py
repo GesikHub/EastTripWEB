b2c22bfbc55cd71da6d4ba945f8ba3b151eed8de
GesikHub <edikaleks43@gmail.com>
2018-05-26 19:29:23 +0300

    time
    time

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,17 +1,21 @@
 from app.api import api
 from flask_restful import Resource
-from app.models import DateMainWindow
 import pytz
 import datetime
+from requests import get
+
 
 
 class MainWindow(Resource):
     def get(self):
         try:
             mytz = pytz.timezone('Europe/Kiev')
-            date = DateMainWindow.query().first()
-            weather = date.weather
-            val = date.euro
+            result = get('https://api.apixu.com/v1/forecast.json?key=0cf0ec96a5eb4feb87b115613180905&q=Kharkiv',
+                         data={}).json()
+            weather = int(result['forecast']['forecastday'][0]['day']['avgtemp_c'])
+            result = get('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json&valcode=EUR',
+                         data={}).json()
+            val = int(result[0]['rate'])
             time = datetime.datetime.now(mytz).hour + ':' + datetime.datetime.now(mytz).minute
             data = datetime.datetime.now(mytz).day
             return {'message': {'weather': weather, 'currency': val, 'time': time, 'data': data}}, 200
--- config.py
+++ config.py
@@ -4,14 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    JOBS = [
-        {
-            'id': 'main_window',
-            'func': 'test:job1',
-            'trigger': 'interval',
-            'seconds': 86400
-        }
-    ]
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
